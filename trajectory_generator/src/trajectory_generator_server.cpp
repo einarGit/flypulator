608e637013b7f907a2b71bdf3737053891ae3c66
Jan Krieglstein <jan.krieglstein@gmail.com>
2018-05-14 18:01:53 +0200

    add polynomial trajectory, rotational correct now

--- trajectory_generator/src/trajectory_generator_server.cpp
+++ trajectory_generator/src/trajectory_generator_server.cpp
@@ -1,5 +1,6 @@
 #include "ros/ros.h"
 #include "trajectory_generator/linear_trajectory.h"
+#include "trajectory_generator/polynomial_trajectory.h"
 //add additional service headers here
 
 // include message structs
@@ -12,11 +13,20 @@
 #include <eigen3/Eigen/Dense> //TODO Why does <Eigen/Dense> not work??
 #include <math.h> //sin,cos functions, M_PI
 
+namespace TrajTypes // own namespace for enumeration following style guide http://wiki.ros.org/CppStyleGuide
+{
+  enum Type
+  {
+     Linear,
+     Polynomial
+     // add new trajectory types here
+  };
+}
 
 ros::Publisher trajectory_publisher; //message publisher for output trajectory, needs to be global to be visible to create<..>Trajectory functions
 
 // convert 2 messages of Vector3 type to 6D float array
-void convertTo6DArray(geometry_msgs::Vector3 x1, geometry_msgs::Vector3 x2, float destination[]){
+void convertTo6DArray(const geometry_msgs::Vector3& x1, const geometry_msgs::Vector3& x2, float destination[]){
     destination[0] = x1.x;
     destination[1] = x1.y;
     destination[2] = x1.z;
@@ -26,164 +36,205 @@ void convertTo6DArray(geometry_msgs::Vector3 x1, geometry_msgs::Vector3 x2, floa
 }
 
 // convert Euler angles to quaternions using roll-pitch-yaw sequence
-Eigen::Quaternionf euler2Quaternion(const float roll, const float pitch, const float yaw){
+void euler2Quaternion(const float roll, const float pitch, const float yaw, Eigen::Quaternionf& q){
     // following https://stackoverflow.com/questions/21412169/creating-a-rotation-matrix-with-pitch-yaw-roll-using-eigen/21414609
     Eigen::AngleAxisf rollAngle (roll, Eigen::Vector3f::UnitX());
     Eigen::AngleAxisf pitchAngle (pitch, Eigen::Vector3f::UnitY());
     Eigen::AngleAxisf yawAngle (yaw, Eigen::Vector3f::UnitZ());
 
-    Eigen::Quaternionf q = yawAngle * pitchAngle * rollAngle;
-    return q; 
+    q = yawAngle * pitchAngle * rollAngle;
 }
 
-Eigen::Matrix3f euler2Rot_dot(const float roll, const float roll_dot, const float pitch, const float pitch_dot, const float yaw, const float yaw_dot){
-    Eigen::Matrix3f R_psi ;
-    R_psi <<  - sin(yaw), -cos(yaw), 0, 
-                cos(yaw), -sin(yaw),0,
-                0,              0,                 0;
-    R_psi = yaw_dot * R_psi;
-
-    Eigen::Matrix3f R_theta;
-    R_theta << -sin(pitch), 0, cos(pitch),
-                0         , 0,             0,
-                -cos(pitch), 0, -sin(pitch);
-    R_theta = pitch_dot * R_theta;
-
-    Eigen::Matrix3f R_phi;
-    R_phi << 0,         0,         0,
-            0, -sin(roll), -cos(roll),
-            0, cos(roll), -sin(roll);
-    R_phi = roll_dot * R_phi;
-
-    Eigen::Matrix3f R_dot = R_psi * R_theta * R_phi; // roll- pitch - yaw sequence, following Janschek lecture RS02_SS16_Vorwaertskinematik.pdf
-    return R_dot;
+// calculate angular velocity from euler angles and its derivatives, following Fje94 p.42
+void calculateOmega(const float roll, const float roll_dot, const float pitch, const float pitch_dot, const float yaw, const float yaw_dot, Eigen::Vector3f& omega){
+    omega.x() = roll_dot - yaw_dot*sin(pitch);
+    omega.y() = pitch_dot*cos(roll) + yaw_dot*sin(roll)*cos(pitch);
+    omega.z() = -pitch_dot*sin(roll) + yaw_dot*cos(roll)*cos(pitch);
 }
 
+// calculate angular acceleration from euler angles and its derivatives, following Fje94 p.42 derivated
+void calculateOmegaDot(const float roll, const float roll_dot, const float roll_ddot,
+                                  const float pitch, const float pitch_dot, const float pitch_ddot,
+                                  const float yaw, const float yaw_dot, const float yaw_ddot, Eigen::Vector3f& omega_dot){
+    omega_dot.x() = roll_ddot - (yaw_ddot*sin(pitch) + yaw_dot*pitch_dot*cos(pitch));
+    omega_dot.y() = pitch_ddot*cos(roll) - pitch_dot*roll_dot*sin(roll) + yaw_ddot*sin(roll)*cos(pitch) + 
+                    yaw_dot*(roll_dot*cos(roll)*cos(pitch) - pitch_dot*sin(roll)*sin(pitch));
+    omega_dot.z() = -(pitch_ddot*sin(roll) + roll_dot*pitch_dot*cos(roll)) + yaw_ddot*cos(roll)*cos(pitch) - 
+                    yaw_dot*(roll_dot*sin(roll)*cos(pitch) + pitch_dot*cos(roll)*sin(pitch));
+}
 
 // create trajectory message 
-trajectory_msgs::MultiDOFJointTrajectoryPoint generateTrajectoryMessage(geometry_msgs::Vector3& x, geometry_msgs::Vector3& x_dot, geometry_msgs::Vector3& x_dotdot, 
-                                                                        geometry_msgs::Quaternion& q, geometry_msgs::Vector3& omega, geometry_msgs::Vector3& omega_dot){
+trajectory_msgs::MultiDOFJointTrajectoryPoint generateTrajectoryMessage(const float p[6], const float p_dot[6], const float p_ddot[6], 
+                                                                        const Eigen::Quaternionf& q, const Eigen::Vector3f& omega, const Eigen::Vector3f& omega_dot, 
+                                                                        const ros::Duration& time_from_start){
     //TODO confirm valid input
     //TODO Maybe pass reference to output to avoid copying of msg_trajectory
 
+    //set linear position, velocity and acceleration in geometry_msgs::Vector3 structs
+    geometry_msgs::Vector3 p_msg;   
+    p_msg.x = p[0];
+    p_msg.y = p[1];
+    p_msg.z = p[2];
+
+    geometry_msgs::Vector3 p_dot_msg;
+    p_dot_msg.x = p_dot[0];
+    p_dot_msg.y = p_dot[1];
+    p_dot_msg.z = p_dot[2];
+
+    geometry_msgs::Vector3 p_ddot_msg;
+    p_ddot_msg.x = p_ddot[0];
+    p_ddot_msg.y = p_ddot[1];
+    p_ddot_msg.z = p_ddot[2];
+
+    geometry_msgs::Quaternion q_msg; // set quaternion struct
+    q_msg.w = q.w();
+    q_msg.x = q.x();
+    q_msg.y = q.y();
+    q_msg.z = q.z();
+
+    geometry_msgs::Vector3 omega_msg; // set omega struct
+    omega_msg.x = omega.x();
+    omega_msg.y = omega.y();
+    omega_msg.z = omega.z();
+
+    geometry_msgs::Vector3 omega_dot_msg; // set omega_dot struct
+    omega_dot_msg.x = omega_dot.x();
+    omega_dot_msg.y = omega_dot.y();
+    omega_dot_msg.z = omega_dot.z();
+
     geometry_msgs::Transform msg_transform; //Pose
-    msg_transform.translation = x;
-    msg_transform.rotation = q;
+    msg_transform.translation = p_msg;
+    msg_transform.rotation = q_msg;
 
     geometry_msgs::Twist msg_velocities; //Velocities
-    msg_velocities.linear = x_dot;
-    msg_velocities.angular = omega;
+    msg_velocities.linear = p_dot_msg;
+    msg_velocities.angular = omega_msg;
 
     geometry_msgs::Twist msg_accelerations; //Accelerations
-    msg_accelerations.linear = x_dotdot;
-    msg_accelerations.angular = omega_dot;
+    msg_accelerations.linear = p_ddot_msg;
+    msg_accelerations.angular = omega_dot_msg;
 
     trajectory_msgs::MultiDOFJointTrajectoryPoint msg_trajectory;
     msg_trajectory.transforms.push_back(msg_transform); //in C++ Ros Message Arrays are implemented as std::vector
     msg_trajectory.velocities.push_back(msg_velocities);
     msg_trajectory.accelerations.push_back(msg_accelerations);
+    msg_trajectory.time_from_start = time_from_start;
 
     return msg_trajectory;
 }
 
-
-bool createLinearTrajectory(trajectory_generator::linear_trajectory::Request &req,
-                            trajectory_generator::linear_trajectory::Response &res){
-    //TODO: type check?
-    //Eigen::Vector3f x_start (req.x_start.x, req.x_start.y, req.x_start.z);
-
+bool createTrajectory(geometry_msgs::Vector3& x_start, geometry_msgs::Vector3& x_end, geometry_msgs::Vector3& rpy_start, geometry_msgs::Vector3& rpy_end, 
+                            float duration, TrajTypes::Type traj_type){
+    // save input values in 6D array
     float pose_start[6];
-    convertTo6DArray(req.x_start, req.rpy_start, pose_start);
+    convertTo6DArray(x_start, rpy_start, pose_start);
     float pose_end[6];
-    convertTo6DArray(req.x_end, req.rpy_end, pose_end);
+    convertTo6DArray(x_end, rpy_end, pose_end);
+   //float duration = req.delta_t; // trajectory duration time
 
-    float pose_current[6];
-
-    float duration = req.delta_t;
-
-    // create current value structs
-    geometry_msgs::Vector3 x_current;
-    geometry_msgs::Vector3 x_dot_current;
-    geometry_msgs::Vector3 x_ddot_current;
-    geometry_msgs::Quaternion q_current_formsg;
-    geometry_msgs::Vector3 omega_current_formsg;
-    geometry_msgs::Vector3 omega_dot_current_formsg;
-
+    float pose_current[6]; // array for 6D pose
+    float vel_current[6]; // array for 6D velocities
+    float acc_current[6]; // array for 6D accelerations
+    // create math objects for quaternion and omega calculations using Eigen library
     Eigen::Quaternionf q_current;
-    Eigen::Matrix3f rotMatrix_current;
     Eigen::Vector3f omega_current;
-    Eigen::Matrix3f rotMatrix_dot_current;
     Eigen::Vector3f omega_dot_current;
-    Eigen::Matrix3f rotMatrix_ddot_current;
 
+    float a[6][6]; // polynomial coefficients for trajectory, first dimension: 
 
-    // calculate constant linear velocity
-    float vel_current[6];
-    for (int i = 0; i<6 ; i++){
-            vel_current[i] = (pose_end[i] - pose_start[i])/duration;
+    // print console message
+    switch (traj_type){
+
+        case TrajTypes::Linear:
+            // calculate constant linear velocity and acceleration (=0)
+            for (int dim = 0; dim<6; dim++){
+                a[dim][0] = pose_start[dim];
+                a[dim][1] = (pose_end[dim] - pose_start[dim])/duration;
+                a[dim][2] = 0;
+                a[dim][3] = 0;
+                a[dim][4] = 0;
+                a[dim][5] = 0;
+            }
+            ROS_INFO("Start linear trajectory..");
+            break;
+
+        case TrajTypes::Polynomial:
+            // calculate polynomial coefficients
+            for (int dim = 0; dim<6; dim++){
+                a[dim][0] = pose_start[dim];
+                a[dim][1] = 0.0f;
+                a[dim][2] = 0.0f;
+                a[dim][3] = 10*(pose_end[dim] - pose_start[dim])/pow(duration,3);
+                a[dim][4] = -15*(pose_end[dim] - pose_start[dim])/pow(duration,4);
+                a[dim][5] = 6*(pose_end[dim] - pose_start[dim])/pow(duration,5);
+            }
+            ROS_INFO("Start polynomial trajectory..");
+            break;
+
+        default:
+            ROS_ERROR("Polynom type not well defined! Must follow enumeration");
     }
-
-    float t_start = 0.0f;
-    float t = t_start;
-    while ( t < t_start + duration ){
-        for (int i = 0; i<6 ; i++){
-            pose_current[i] = vel_current[i] * t + pose_start[i];
+    
+    const ros::Time t_start = ros::Time::now(); // TODO: take ros time here
+    const ros::Duration trajDuration (duration);
+    ros::Time t (t_start.toSec()); // running time
+    ROS_INFO("Start Time: %f", t.toSec());
+    ros::Rate r(10); //1Hz
+    // start continous message publishing 
+    while ( t <= t_start + trajDuration ){
+        // calculate x(t)
+        if (traj_type == TrajTypes::Linear || traj_type == TrajTypes::Polynomial)
+        {
+            // linear trajectory is also a polynomial trajectory with different coeffiencts (set before)
+            float dt = (float) (t.toSec() - t_start.toSec());
+            for (int dim = 0; dim < 6; dim++){
+                pose_current[dim] = a[dim][0] + a[dim][1]*dt + a[dim][2]*pow(dt,2) + a[dim][3]*pow(dt,3) + a[dim][4]*pow(dt,4) + a[dim][5]*pow(dt,5);
+                vel_current[dim] = a[dim][1] + 2*a[dim][2]*dt + 3*a[dim][3]*pow(dt,2) + 4*a[dim][4]*pow(dt,3) + 5*a[dim][5]*pow(dt,4);
+                acc_current[dim] = 2*a[dim][2] + 6*a[dim][3]*dt + 12*a[dim][4]*pow(dt,2) + 20*a[dim][5]*pow(dt,3);
+            }
         }
-        //set linear velocities in geometry_msgs::Vector3 structs
-        geometry_msgs::Vector3 x_current;
-        x_current.x = pose_current[0];
-        x_current.y = pose_current[1];
-        x_current.z = pose_current[2];
-        x_dot_current.x = vel_current[0];
-        x_dot_current.y = vel_current[1];
-        x_dot_current.z = vel_current[2];
-        x_ddot_current.x = 0;
-        x_ddot_current.y = 0;
-        x_ddot_current.z = 0;
+        // add other trajectory types here
+       
+        // vel_current and acc_current remain constant on linear trajectory, so no need for calculation
+        // calculate quaternion from euler angles
+        euler2Quaternion(pose_current[3], pose_current[4], pose_current[5], q_current);
+        // calculate omega from euler angles and their derivative
+        calculateOmega(pose_current[3], vel_current[3], pose_current[4], vel_current[4], pose_current[5], vel_current[5], omega_current);
+        // calculate omega_dot from euler angles and their derivatives
+        calculateOmegaDot(pose_current[3], vel_current[3], acc_current[3],
+                          pose_current[4], vel_current[4], acc_current[4],
+                          pose_current[5], vel_current[5], acc_current[5], omega_dot_current);
 
-        //calculate quaternion from euler angles
-        q_current = euler2Quaternion(pose_current[3], pose_current[4], pose_current[5]);
-        rotMatrix_current = q_current.toRotationMatrix(); //calculate rotation matrix
-        //calculate first derivative of rotation matrix
-        rotMatrix_dot_current = euler2Rot_dot(pose_current[3], vel_current[3], pose_current[4], vel_current[4], pose_current[5], vel_current[5]);
-        Eigen::Matrix3f tmp_matrix = rotMatrix_current.transpose() * rotMatrix_dot_current;
+        // Concatenate informations to trajectory message
+        trajectory_msgs::MultiDOFJointTrajectoryPoint msg = generateTrajectoryMessage(pose_current, vel_current, acc_current, 
+                                                                                    q_current, omega_current, omega_dot_current, ros::Duration(t-t_start));
+        trajectory_publisher.publish(msg); //publish message to topic /trajectory
+
+         //TODO sleep 1/frequency of Pose estimation
         
-        omega_dot_current = Eigen::Vector3f(0,0,0);
-
-        q_current_formsg.w = q_current.w();
-        q_current_formsg.x = q_current.x();
-        q_current_formsg.y = q_current.y();
-        q_current_formsg.z = q_current.z();
-
-        omega_current_formsg.x = tmp_matrix.coeff(3,2);//omega_current.x();
-        omega_current_formsg.y = tmp_matrix.coeff(1,3);//omega_current.y();
-        omega_current_formsg.z = tmp_matrix.coeff(2,1);//omega_current.z();
-
-        omega_dot_current_formsg.x = omega_dot_current.x();
-        omega_dot_current_formsg.y = omega_dot_current.y();
-        omega_dot_current_formsg.z = omega_dot_current.z();
-
-        trajectory_msgs::MultiDOFJointTrajectoryPoint msg = generateTrajectoryMessage(x_current, x_dot_current, x_ddot_current, 
-                                                                                    q_current_formsg, omega_current_formsg, omega_dot_current_formsg);
-        trajectory_publisher.publish(msg);
-        t = t + 0.1;
+        ros::spinOnce(); // not necessary (?) but good measure
+        r.sleep();
+        t = ros::Time::now();
+        ROS_INFO("Current Time: %f", t.toSec());
     }
 
+    ROS_INFO("trajectory finished!");
 
-
-    res.finished = true;
-
-    //ROS_INFO("request: x_start = %2.2f, x_end = %2.2f, duration = %2.2f", x_start,x_end,duration);
-
-
-
-
-    ros::spinOnce(); // not necessary but good measure
     return true;
 }
 
 
+bool createPolynomialTrajectory(trajectory_generator::polynomial_trajectory::Request &req,
+                            trajectory_generator::polynomial_trajectory::Response &res){
+    res.finished = createTrajectory(req.x_start, req.x_end, req.rpy_start, req.rpy_end, req.delta_t,TrajTypes::Polynomial);
+    return true;
+}
 
+bool createLinearTrajectory(trajectory_generator::linear_trajectory::Request &req,
+                            trajectory_generator::linear_trajectory::Response &res){
+    res.finished = createTrajectory(req.x_start, req.x_end, req.rpy_start, req.rpy_end, req.delta_t,TrajTypes::Linear);
+    return true;
+}
 
 
 int main(int argc, char **argv)
@@ -193,7 +244,9 @@ int main(int argc, char **argv)
 
     //register service
     ros::ServiceServer serviceLinTraj = n.advertiseService("linear_trajectory",createLinearTrajectory);
-    ROS_INFO("Service linear_trajectory ready");
+    ROS_INFO("service linear_trajectory ready");
+    ros::ServiceServer servicePolTraj = n.advertiseService("polynomial_trajectory",createPolynomialTrajectory);
+    ROS_INFO("service polynomial_trajectory ready");
 
     //register publisher for output message
     trajectory_publisher = n.advertise<trajectory_msgs::MultiDOFJointTrajectoryPoint>("trajectory",1000);
